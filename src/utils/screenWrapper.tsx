acae70c7da63056f94eaa709b061407859a0b795
Yash <yashzariya.offical@gmail.com>
2026-05-15 15:46:36 +0530

    SafeArea view problem solved on android

--- src/utils/screenWrapper.tsx
+++ src/utils/screenWrapper.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Platform, StatusBar } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const ScreenWrapper = ({ children, style }) => {
   return (
-    <SafeAreaView style={[{ flex: 1 }, style]} edges={['top']}>
+    <SafeAreaView
+      style={[
+        {
+          flex: 1,
+          paddingTop: Platform.OS === 'android' ? 0 : 0,
+        },
+        style,
+      ]}
+      edges={Platform.OS === 'ios' ? ['top'] : []}
+    >
       {children}
     </SafeAreaView>
   );
